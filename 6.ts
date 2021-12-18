1fb56637b343d8d6dfe182bbfc2e55e16bf5c00b
AJ Coppa <aj@chartio.com>
2021-12-18 17:39:41 -0500

    6**

--- 6.ts
+++ 6.ts
@@ -9,31 +9,61 @@ async function main() {
   partTwo(fish);
 }
 
-function partOne(fish: number[]) {
+function partOne(fish: Fish) {
   let updatedFish = tick(fish, 80);
-  console.log(updatedFish.length);
+  console.log(count(updatedFish));
 }
 
-function partTwo(fish: number[]) {}
+function partTwo(fish: Fish) {
+  let updatedFish = tick(fish, 256);
+  console.log(count(updatedFish));
+}
 
-function tick(fish: number[], times: number): number[] {
+// number from 0-8 => count of fish at that stage
+type Fish = Map<number, number>;
+
+function tick(fish: Fish, times: number): Fish {
   if (times === 0) {
     return fish;
   }
-  const newCount = fish.filter((n) => n === 0).length;
-  let updatedFish = fish.map((n) => (n === 0 ? 6 : n - 1));
-  for (let i = 0; i < newCount; i++) {
-    updatedFish.push(8);
+
+  let newFish: Map<number, number> = cloneMap(fish);
+  const newCount = newFish.get(0) || 0;
+  for (let i = 0; i <= 7; i++) {
+    newFish.set(i, newFish.get(i + 1) || 0);
   }
-  return tick(updatedFish, times - 1);
+  newFish.set(8, newCount);
+  newFish.set(6, (newFish.get(6) || 0) + newCount);
+  return tick(newFish, times - 1);
 }
 
-function logFish(fish: number[]): void {
-  console.log(fish.join(","));
+function logFish(fish: Fish): void {
+  for (let i = 0; i <= 8; i++) {
+    process.stdout.write(`${fish.get(i) || 0} `);
+  }
+  process.stdout.write("\n");
 }
 
-function parseLines(lines: string[]): number[] {
-  return lines[0].split(",").map((s) => parseInt(s, 10));
+function count(fish: Fish): number {
+  let total = 0;
+  for (const val of fish.values()) {
+    total += val;
+  }
+  return total;
+}
+
+function parseLines(lines: string[]): Fish {
+  const numbers = lines[0].split(",").map((s) => parseInt(s, 10));
+  const fish = new Map<number, number>();
+  numbers.forEach((n) => {
+    const countAtN = fish.get(n) || 0;
+    fish.set(n, countAtN + 1);
+  });
+  return fish;
+}
+
+function cloneMap(x: Map<any, any>): Map<any, any> {
+  return new Map(JSON.parse(JSON.stringify(Array.from(x))));
 }
 
 main();
