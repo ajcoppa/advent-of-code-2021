9e313934c81063a2d2ea2114b5e6607f4a487be4
AJ Coppa <aj@chartio.com>
2021-12-18 19:39:54 -0500

    7**

--- 7.ts
+++ 7.ts
@@ -6,7 +6,7 @@ async function main() {
   const lines = await loadFromFile("7-input.txt");
   const positions = parseLines(lines);
   partOne(positions);
-  // partTwo(positions);
+  partTwo(positions);
 }
 
 function partOne(positions: number[]) {
@@ -18,10 +18,34 @@ function partOne(positions: number[]) {
   console.log(Math.min(...fuelCosts));
 }
 
-function partTwo(positions: number[]) {}
+function partTwo(positions: number[]) {
+  const maxPosition = Math.max(...positions);
+  let fuelCosts: number[] = [];
+  for (let i = 0; i <= maxPosition; i++) {
+    fuelCosts.push(totalDistanceToX(positions, i, true));
+  }
+  console.log(Math.min(...fuelCosts));
+}
 
-function totalDistanceToX(positions: number[], x: number): number {
-  return sum(positions.map((pos) => Math.abs(pos - x)));
+function totalDistanceToX(
+  positions: number[],
+  x: number,
+  increasingFuelCost: boolean = false
+): number {
+  return sum(
+    positions.map((pos) => {
+      const distance = Math.abs(pos - x);
+      return increasingFuelCost ? triangularNumber(distance) : distance;
+    })
+  );
+}
+
+function triangularNumber(x: number): number {
+  let total = 0;
+  for (let i = x; i > 0; i--) {
+    total += i;
+  }
+  return total;
 }
 
 function parseLines(lines: string[]): number[] {
