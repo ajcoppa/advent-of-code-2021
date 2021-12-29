daddcd66a7d2544416411f45b262fe316b50f83e
AJ Coppa <aj@chartio.com>
2021-12-29 11:45:59 -0500

    10**

--- 10.ts
+++ 10.ts
@@ -10,20 +10,36 @@ async function main() {
 
 function partOne(lines: string[]) {
   const results = lines.map((line) => {
-    const result = parseExpression(line);
-    return pointValue(result.encountered);
+    const parsed = parseExpression(line);
+    const firstError = parsed?.errors[0];
+    if (firstError !== undefined) {
+      return errorPointValue(firstError.encountered);
+    } else {
+      return 0;
+    }
   });
   const sum = results.reduce((a, b) => a + b);
   console.log(sum);
 }
 
-function partTwo(lines: string[]) {}
+function partTwo(lines: string[]) {
+  const results = lines
+    .map((line) => {
+      const parsed = parseExpression(line);
+      if (parsed.errors.length === 0) {
+        return scoreCharsToComplete(parsed.charsToComplete);
+      } else {
+        return 0;
+      }
+    })
+    .filter((x) => x !== 0);
+  const middleIndex = Math.floor(results.length / 2);
+  console.log(results.sort((a, b) => a - b)[middleIndex]);
+}
 
-function parseExpression(line: string): {
-  encountered: string;
-  expected: string;
-} {
+function parseExpression(line: string): ParsedExpression {
   let stack: string[] = [];
+  let errors: Error[] = [];
   for (let i = 0; i < line.length; i++) {
     const c = line[i];
 
@@ -32,16 +48,22 @@ function parseExpression(line: string): {
     } else if (isClosing(c)) {
       const last = stack[stack.length - 1];
       const opener = openerForCloser(c);
-      if (last === opener) {
-        stack.pop();
-      } else {
-        return { encountered: c, expected: closerForOpener(last) };
+      if (last !== opener) {
+        errors.push({ encountered: c, expected: closerForOpener(last) });
       }
+      stack.pop();
     }
   }
 
-  return { encountered: "", expected: "" };
+  const charsToComplete = stack.map(closerForOpener).reverse().join("");
+  return { errors, charsToComplete };
 }
+
+type Error = { encountered: string; expected: string };
+type ParsedExpression = {
+  errors: Error[];
+  charsToComplete: string;
+};
 
 function isOpening(c: string): boolean {
   if (["(", "[", "{", "<"].includes(c)) {
@@ -89,7 +111,7 @@ function closerForOpener(c: string): string {
   }
 }
 
-function pointValue(c: string): number {
+function errorPointValue(c: string): number {
   switch (c) {
     case ")":
       return 3;
@@ -102,6 +124,30 @@ function pointValue(c: string): number {
     default:
       return 0;
   }
+}
+
+function completePointValue(c: string): number {
+  switch (c) {
+    case ")":
+      return 1;
+    case "]":
+      return 2;
+    case "}":
+      return 3;
+    case ">":
+      return 4;
+    default:
+      return 0;
+  }
+}
+
+function scoreCharsToComplete(s: string): number {
+  let score = 0;
+  for (const i in s.split("")) {
+    score *= 5;
+    score += completePointValue(s[i]);
+  }
+  return score;
 }
 
 main();
